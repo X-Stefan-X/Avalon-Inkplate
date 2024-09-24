075407263e5776b3c697f5faf0f2c11eb38c70d1
StefanKrax <stefan.krax@betasystems.com>
2024-09-24 09:42:25 +0200

    Version 0.3 Other WiFi and more Data to display with hopefully correct conversion.

--- examples/2. Advanced Inkplate Features/1-Inkplate_WiFi_HTTP_request/1-Inkplate_WiFi_HTTP_request.cpp
+++ examples/2. Advanced Inkplate Features/1-Inkplate_WiFi_HTTP_request/1-Inkplate_WiFi_HTTP_request.cpp
@@ -33,22 +33,36 @@ Inkplate display(INKPLATE_1BIT);    //Create an object on Inkplate library and a
 void initDisplayText() {
   display.clearDisplay();
   display.setTextSize(3);
-  display.setCursor(0, 0);
-  display.print("Tiefe");
-  display.setCursor(400, 0);
-  display.print("TWA");
-  display.setCursor(0, 300);
-  display.print("SOG");
-  display.setCursor(400, 300);
-  display.print("TWS");
+  display.setCursor(5, 0);
+  display.print("Tiefe in m");
+  display.setCursor(305, 0);
+  display.print("COG in Grad");
+  display.drawThickLine(0, 195, 600, 195, 1, 1);
+  display.setCursor(5, 200);
+  display.print("STW in kn");
+  display.setCursor(305, 200);
+  display.print("SOG in kn");
+  display.drawThickLine(0, 395, 600, 395, 1, 1);
+  display.drawThickLine(299, 0, 299, 800, 1, 2);
+
+  display.setCursor(5, 400);
+  display.print("Luftdruck");
+  display.setCursor(305, 400);
+  display.print("Wassertemp in Grad");
+   display.drawThickLine(0, 595, 600, 595, 1, 1);
+  display.setCursor(5, 600);
+  display.print("Trip Log");
+  display.setCursor(305, 600);
+  display.print("Position");
   display.display();
 }
 
 void setup() {
   display.begin();                  //Init Inkplate library (you should call this function ONLY ONCE)
   display.clearDisplay();           //Clear frame buffer of display
+  display.setRotation(1);
   display.display();                //Put clear image on display
-  display.setTextSize(2);           //Set text scaling to two (text will be two times bigger)
+  display.setTextSize(3);           //Set text scaling to two (text will be two times bigger)
   display.setCursor(0, 0);          //Set print position
   display.setTextColor(BLACK, WHITE);                 //Set text color to black and background color to white
   display.println("Scanning for WiFi networks...");    //Write text
@@ -84,13 +98,13 @@ void setup() {
   display.partialUpdate();
   delay(1000);                                        //Wait for a second
   display.clearDisplay();                             //Clear everything in frame buffer
-  display.setCursor(100,160);
-  display.setTextSize(10);                             //Set print cursor to new position
+  display.setCursor(50, 300);
+  display.setTextSize(8);                             //Set print cursor to new position
   display.println("Willkommen");                       
-  display.setCursor(80, 300);
+  display.setCursor(30, 400);
   display.println("auf Avalon!");
   display.display();                                  //Refresh the screen
-  delay(5000);                                        //Wait for 2 seconds
+  delay(3000);                                        //Wait for 2 seconds
   display.clearDisplay();                             //Clear everything in frame buffer
 
   initDisplayText(); 
@@ -98,10 +112,10 @@ void setup() {
 
 
 void ErrorHandlingValues(String ErrorText, int Postion_x, int Postion_y) {
-  display.setCursor(Postion_x, Postion_y + 5);
+  display.setCursor(Postion_x, Postion_y);
   display.setTextSize(2);
-  display.print("deserializeJson() failed: ");
-  display.setCursor(Postion_x +10, Postion_y + 5);
+  display.print("deserializeJson failed");
+  display.setCursor(Postion_x, Postion_y + 20);
   display.print(ErrorText);
   display.partialUpdate();
 }
@@ -118,14 +132,56 @@ void ValueAndDisplayHandling(HTTPClient &http, int JsonLength, int Postion_x, in
       else
       {
       const char *meta_description = doc["meta"]["description"]; // "Depth related data"
-      double value = doc["value"];                               // 245.01020000000003
+      const char *value = doc["value"];                               // 245.01020000000003
       const char *source = doc["$source"];                       // "simulator.0"
       const char *timestamp = doc["timestamp"];                  // "2024-04-26T18:24:41.384Z"
+      const char *unit = doc["unit"];                            // "m"
+      const char *path= doc["path"];                             // "environment.depth.belowTransducer"
 
       // Print values
       display.setCursor(Postion_x, Postion_y);
-      display.setTextSize(10);
-      display.print(value);
+      if (path == "navigation.position") {
+        // Latitude and Longitude are ptinted in two lines
+        const char* positionstring = strtok(strdup(value), ",");
+        display.setTextSize(2);
+        display.print("Lat: ");
+        display.print(value);
+        display.setCursor(Postion_x, Postion_y + 20);
+        display.print("Lon: ");
+        display.print(value);
+      } else {
+        // All other values are printed in one line
+        try {
+          // Try to convert the value to a number
+          double num = atof(value);
+          if (unit =="rad") {
+            num = num * RAD_TO_DEG;
+            unit = "°";
+          } else if (unit == "m/s") {
+            num = num * 1.94384;
+            unit = "kn";
+          } else if (unit == "Pa") {
+            num = num / 100;
+            unit = "hPa";
+          } else if (unit == "m") {
+            num = num * 0.00053996;
+            unit = "Nm";
+          } else if (unit == "K") { 
+            num = num - 273.15;
+            unit = "°C";
+          } else if (unit == "ratio") {
+            num = num * 100;
+            unit = "%";
+          }  
+          display.setTextSize(12);
+          display.printf("0.4f", num);
+        } catch (const std::invalid_argument& e) {  
+          display.setTextSize(12);
+          display.printf("0.4f", value);
+        }
+                
+      }
+      // Put the timestamp on the next line
       display.setCursor(Postion_x, Postion_y + 100);
       display.setTextSize(1);
       display.print(timestamp);
@@ -157,20 +213,36 @@ void loop() {
   }
   
   //Depth
-  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/environment/depth/")) {
-    ValueAndDisplayHandling(http, 192, 0, 30); 
+  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/environment/depth/belowTransducer/")) {
+    ValueAndDisplayHandling(http, 192, 5, 30); 
     }
-  // TWA
-  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/environment/twa/")) {
-    ValueAndDisplayHandling(http, 192, 400, 30);
+  // COG
+  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/navigation/courseOverGroundTrue/")) {
+    ValueAndDisplayHandling(http, 192, 305, 30);
     }
-//Speed 
-if(http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/navigation/speedOverGround/")) {
-  ValueAndDisplayHandling(http, 192, 0, 330);
+//STW 
+if(http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/navigation/speedThroughWater/")) {
+  ValueAndDisplayHandling(http, 192, 5, 230);
   } 
-  //tws
-  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/environment/tws/")) {
-    ValueAndDisplayHandling(http, 192, 400, 330);
+  //SOG
+  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/navigation/speedOverGround/")) {
+    ValueAndDisplayHandling(http, 192, 305, 230);
+  }
+//environment.outside.pressure
+  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/environment/outside/pressure/")) {
+    ValueAndDisplayHandling(http, 192, 5, 430);
+  }
+//environment.water.temperature
+  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/environment/water/temperature/")) {
+    ValueAndDisplayHandling(http, 192, 305, 430);
+  }
+//navigation.trip.log 
+  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/navigation/trip/log/")) {
+    ValueAndDisplayHandling(http, 192, 5, 630);
+  }
+//navigation.position
+  if (http.begin("http://openplotter:3000/signalk/v1/api/vessels/self/navigation/position/")) {
+    ValueAndDisplayHandling(http, 192, 305, 630);
   }
 
 //http.end();
